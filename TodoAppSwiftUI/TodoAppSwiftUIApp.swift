740ff198f377a629e565b41e55c18ab6a85c07a3
jonasbergstrom <jontrian97@gmail.com>
2022-09-22 21:06:47 +0200

    Getting access to the "CoreDataManager" and "managedObjectContext" from any view

--- TodoAppSwiftUI/TodoAppSwiftUIApp.swift
+++ TodoAppSwiftUI/TodoAppSwiftUIApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct TodoAppSwiftUIApp: App {
+    
+    let persistentContainer = CoreDataManager.shared.persistentContainer
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environment(\.managedObjectContext, persistentContainer.viewContext)
         }
     }
 }
