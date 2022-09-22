90c25c15faf3cd71d7521d8d5c7a2e4ff5f288fb
jonasbergstrom <jontrian97@gmail.com>
2022-09-22 20:38:27 +0200

    Initial Commit

---
+++ TodoAppSwiftUI/TodoAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppSwiftUIApp.swift
+//  TodoAppSwiftUI
+//
+//  Created by Jonas Bergström on 2022-09-22.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
